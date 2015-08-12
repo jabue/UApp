6637847f3841b48bf2af34e39a05727991e6dc29
rafy zhao <lafeike@gmail.com>
2015-08-12 07:20:41 -0700

    add ios part developed by David

---
+++ ios/uapp/UIPageViewController/ViewController.swift
@@ -0,0 +1,138 @@
+//
+//  ViewController.swift
+//  UIPageViewController
+//
+//  Created by PJ Vea on 3/27/15.
+//  Copyright (c) 2015 Vea Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIPageViewControllerDataSource {
+
+    var pageViewController: UIPageViewController!
+    var pageTitles: NSArray!
+    var pageImages: NSArray!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+       
+        self.pageTitles = NSArray(objects: "App", "Campus","Welcome","EVTech")
+        self.pageImages = NSArray(objects: "page1", "page2","page3","page4")
+
+        self.pageViewController = self.storyboard?.instantiateViewControllerWithIdentifier("PageViewController") as UIPageViewController
+        self.pageViewController.dataSource = self
+
+        var startVC = self.viewControllerAtIndex(0) as ContentViewController
+        var viewControllers = NSArray(object: startVC)
+        
+        self.pageViewController.setViewControllers(viewControllers as [AnyObject], direction: .Forward, animated: true, completion: nil)
+        
+        self.pageViewController.view.frame = CGRectMake(0, 30, self.view.frame.width, self.view.frame.size.height - 60)
+        
+        self.addChildViewController(self.pageViewController)
+        self.view.addSubview(self.pageViewController.view)
+        self.pageViewController.didMoveToParentViewController(self)
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func restartAction(sender: AnyObject)
+    {
+        //var startVC = self.viewControllerAtIndex(0) as ContentViewController
+        //var viewControllers = NSArray(object: startVC)
+        
+        //self.pageViewController.setViewControllers(viewControllers as [AnyObject], direction: .Forward, animated: true, completion: nil)
+       
+    }
+    
+    func viewControllerAtIndex(index: Int) -> ContentViewController
+    {
+        if ((self.pageTitles.count == 0) || (index >= self.pageTitles.count)) {
+            return ContentViewController()
+        }
+        
+        var vc: ContentViewController = self.storyboard?.instantiateViewControllerWithIdentifier("ContentViewController") as ContentViewController
+        
+        vc.imageFile = self.pageImages[index] as String
+        vc.titleText = self.pageTitles[index] as String
+        vc.pageIndex = index
+        
+        return vc
+        
+        
+    }
+    
+    
+    // MARK: - Page View Controller Data Source
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController?
+    {
+        
+        var vc = viewController as ContentViewController
+        var index = vc.pageIndex as Int
+        
+        
+        if (index == 0 || index == NSNotFound)
+        {
+            return nil
+            
+        }
+        
+        index--
+        return self.viewControllerAtIndex(index)
+        
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        
+        var vc = viewController as ContentViewController
+        var index = vc.pageIndex as Int
+        
+        if (index == NSNotFound)
+        {
+            return nil
+        }
+        
+        index++
+        
+        if (index == self.pageTitles.count)
+        {   
+       
+
+            return nil
+            
+            
+        }
+        
+        
+        if (index  > self.pageTitles.count){
+        
+          self.performSegueWithIdentifier("myResultBoard", sender: self)
+            
+          
+        }
+        
+        
+        return self.viewControllerAtIndex(index)
+  
+    }
+    
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int
+    {
+        return self.pageTitles.count
+    }
+
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int
+    {
+        return 0
+    }
+
+}
+
