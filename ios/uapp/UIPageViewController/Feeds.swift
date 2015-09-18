bdaa20c2865b94743a7b619de7b08af5462473d7
zihan901015 <284807225@qq.com>
2015-09-18 14:04:37 -0700

    just commit

---
+++ ios/uapp/UIPageViewController/Feeds.swift
@@ -0,0 +1,9 @@
+//
+//  Feeds.swift
+//  UIPageViewController
+//
+//  Created by joey on 15/9/17.
+//  Copyright © 2015年 Vea Software. All rights reserved.
+//
+
+import Foundation
---
+++ ios/uapp/UIPageViewController/HotTopic.swift
@@ -0,0 +1,9 @@
+//
+//  HotTopic.swift
+//  UIPageViewController
+//
+//  Created by joey on 15/9/17.
+//  Copyright © 2015年 Vea Software. All rights reserved.
+//
+
+import Foundation
